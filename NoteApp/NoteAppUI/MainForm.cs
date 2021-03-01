e5c264090a9c7f23cd5f5a891a645d347962d123
alister.teyrin@inbox.ru <alister.teyrin@inbox.ru>
2021-03-01 18:20:24 +0300

    Добавлен проект логики

--- NoteApp/NoteAppUI/MainForm.cs
+++ NoteApp/NoteAppUI/MainForm.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NoteApp;
 
 namespace NoteAppUI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
