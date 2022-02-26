88dc6487634b3b8df53cf28aef79d03778f64835
alister.teyrin@inbox.ru <alister.teyrin@inbox.ru>
2022-02-27 01:15:27 +0300

    Добавлено тестовое поле

---
+++ NoteApp/NoteAppUnitTests/NoteTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using NoteApp;
+
+namespace NoteAppUnitTests
+{
+    [TestFixture]
+    public class NoteTest
+    {
+        private Note Note { set; get; }
+
+        [Test(Description = "")]
+        public void Get_Name()
+        {
+            //setup
+            Note = new Note();
+            var expected = "test";
+            Note.Name = expected;
+
+            //act
+            var actual = Note.Name;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ NoteApp/NoteAppUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NoteAppUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoteAppUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("825e6994-8dad-4daa-b512-f10bbc1ef41a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
